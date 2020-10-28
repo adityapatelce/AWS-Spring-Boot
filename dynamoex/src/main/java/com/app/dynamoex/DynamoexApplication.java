159de91f1d73b1b6da879fe829bc076a000b52b3
adityadevpatel <adityapatel9173@gmail.com>
2020-10-28 19:36:31 +0530

    Project init

---
+++ dynamoex/src/main/java/com/app/dynamoex/DynamoexApplication.java
@@ -0,0 +1,13 @@
+package com.app.dynamoex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoexApplication.class, args);
+	}
+
+}
---
+++ dynamoex/src/test/java/com/app/dynamoex/DynamoexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dynamoex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
